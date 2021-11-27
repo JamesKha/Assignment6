a82f0efba092710ec8e26f3605776be2d021bcfc
Jimmy <jimmy.kha@mytwu.ca>
2021-11-27 15:13:57 -0800

    Original Shaded Cube

--- Assignment6/Assignment6/Assignment6.js
+++ Assignment6/Assignment6/Assignment6.js
@@ -1,95 +1,176 @@
 "use strict";
 
+var shadedCube = function() {
+
 var canvas;
 var gl;
+
+var numPositions = 36;
+
+var positionsArray = [];
+var normalsArray = [];
+
+var vertices = [
+        vec4(-0.5, -0.5,  0.5, 1.0),
+        vec4(-0.5,  0.5,  0.5, 1.0),
+        vec4(0.5,  0.5,  0.5, 1.0),
+        vec4(0.5, -0.5,  0.5, 1.0),
+        vec4(-0.5, -0.5, -0.5, 1.0),
+        vec4(-0.5,  0.5, -0.5, 1.0),
+        vec4(0.5,  0.5, -0.5, 1.0),
+        vec4(0.5, -0.5, -0.5, 1.0)
+    ];
+
+var lightPosition = vec4(1.0, 1.0, 1.0, 0.0);
+var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
+var lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
+var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
+
+var materialAmbient = vec4(1.0, 0.0, 1.0, 1.0);
+var materialDiffuse = vec4(1.0, 0.8, 0.0, 1.0);
+var materialSpecular = vec4(1.0, 0.8, 0.0, 1.0);
+var materialShininess = 100.0;
+
+var ctm;
+var ambientColor, diffuseColor, specularColor;
+var modelViewMatrix, projectionMatrix;
+var viewerPos;
+var program;
 
 var xAxis = 0;
 var yAxis = 1;
 var zAxis = 2;
-
 var axis = 0;
-var theta = [ 0, 0, 0 ];
+var theta = vec3(0, 0, 0);
 
 var thetaLoc;
 
 var flag = false;
 
-var points = [];
-var colors = [];
+function quad(a, b, c, d) {
 
-window.onload = function init()
+     var t1 = subtract(vertices[b], vertices[a]);
+     var t2 = subtract(vertices[c], vertices[b]);
+     var normal = cross(t1, t2);
+     normal = vec3(normal);
+
+
+     positionsArray.push(vertices[a]);
+     normalsArray.push(normal);
+     positionsArray.push(vertices[b]);
+     normalsArray.push(normal);
+     positionsArray.push(vertices[c]);
+     normalsArray.push(normal);
+     positionsArray.push(vertices[a]);
+     normalsArray.push(normal);
+     positionsArray.push(vertices[c]);
+     normalsArray.push(normal);
+     positionsArray.push(vertices[d]);
+     normalsArray.push(normal);
+}
+
+
+function colorCube()
 {
-    canvas = document.getElementById( "gl-canvas" );
+    quad(1, 0, 3, 2);
+    quad(2, 3, 7, 6);
+    quad(3, 0, 4, 7);
+    quad(6, 5, 1, 2);
+    quad(4, 5, 6, 7);
+    quad(5, 4, 0, 1);
+}
+
+
+window.onload = function init() {
+    canvas = document.getElementById("gl-canvas");
 
     gl = canvas.getContext('webgl2');
-    if (!gl) { alert( "WebGL 2.0 isn't available" ); }
-
-    var myCube =  cube();
-    var myCube2 = cube(1.0);
-
-    myCube.scale(0.5, 0.5, 0.5);
-    myCube2.scale(0.5, 0.5, 0.5);
-    myCube.rotate(45, [1, 1, 1]);
-    myCube.translate(0.5, 0.5, 0.0);
-    myCube2.translate(-0.5, -0.5, 0.0);
+    if (!gl) alert( "WebGL 2.0 isn't available");
 
 
-    colors = myCube.TriangleVertexColors;
-    points = myCube.TriangleVertices;
-    points = points.concat(myCube2.TriangleVertices);
-    colors = colors.concat(myCube2.TriangleFaceColors);
-
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
     gl.enable(gl.DEPTH_TEST);
-    //gl.enable(gl.CULL);
 
     //
     //  Load shaders and initialize attribute buffers
     //
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
 
-    var cBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
+    colorCube();
 
-    var colorLoc = gl.getAttribLocation( program, "aColor" );
-    gl.vertexAttribPointer( colorLoc, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( colorLoc );
+    var nBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
+
+    var normalLoc = gl.getAttribLocation(program, "aNormal");
+    gl.vertexAttribPointer(normalLoc, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(normalLoc);
 
     var vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(positionsArray), gl.STATIC_DRAW);
 
-
-    var positionLoc = gl.getAttribLocation( program, "aPosition" );
-    gl.vertexAttribPointer( positionLoc, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( positionLoc );
+    var positionLoc = gl.getAttribLocation(program, "aPosition");
+    gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(positionLoc);
 
     thetaLoc = gl.getUniformLocation(program, "theta");
 
-    //event listeners for buttons
+    viewerPos = vec3(0.0, 0.0, -20.0);
 
-    document.getElementById( "xButton" ).onclick = function () {
-        axis = xAxis;
-    };
-    document.getElementById( "yButton" ).onclick = function () {
-        axis = yAxis;
-    };
-    document.getElementById( "zButton" ).onclick = function () {
-        axis = zAxis;
-    };
+    projectionMatrix = ortho(-1, 1, -1, 1, -100, 100);
+
+    var ambientProduct = mult(lightAmbient, materialAmbient);
+    var diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    var specularProduct = mult(lightSpecular, materialSpecular);
+
+    document.getElementById("ButtonX").onclick = function(){axis = xAxis;};
+    document.getElementById("ButtonY").onclick = function(){axis = yAxis;};
+    document.getElementById("ButtonZ").onclick = function(){axis = zAxis;};
     document.getElementById("ButtonT").onclick = function(){flag = !flag;};
+
+    gl.uniform4fv(gl.getUniformLocation(program, "uAmbientProduct"),
+       ambientProduct);
+    gl.uniform4fv(gl.getUniformLocation(program, "uDiffuseProduct"),
+       diffuseProduct );
+    gl.uniform4fv(gl.getUniformLocation(program, "uSpecularProduct"),
+       specularProduct );
+    gl.uniform4fv(gl.getUniformLocation(program, "uLightPosition"),
+       lightPosition );
+
+    gl.uniform1f(gl.getUniformLocation(program,
+       "uShininess"), materialShininess);
+
+    gl.uniformMatrix4fv( gl.getUniformLocation(program, "uProjectionMatrix"),
+       false, flatten(projectionMatrix));
     render();
 }
 
-function render()
-{
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+var render = function(){
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
     if(flag) theta[axis] += 2.0;
-    gl.uniform3fv(thetaLoc, theta);
-    gl.drawArrays( gl.TRIANGLES, 0, points.length);
-    requestAnimationFrame( render );
+
+    modelViewMatrix = mat4();
+    modelViewMatrix = mult(modelViewMatrix, rotate(theta[xAxis], vec3(1, 0, 0)));
+    modelViewMatrix = mult(modelViewMatrix, rotate(theta[yAxis], vec3(0, 1, 0)));
+    modelViewMatrix = mult(modelViewMatrix, rotate(theta[zAxis], vec3(0, 0, 1)));
+
+    //console.log(modelView);
+
+    gl.uniformMatrix4fv(gl.getUniformLocation(program,
+            "uModelViewMatrix"), false, flatten(modelViewMatrix));
+
+    gl.drawArrays(gl.TRIANGLES, 0, numPositions);
+
+
+    requestAnimationFrame(render);
 }
+
+}
+
+shadedCube();
